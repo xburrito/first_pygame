3efebc37053cdf6a5d011242ae430ddb5a7ff7c8
Albert <50990038+xburrito@users.noreply.github.com>
2020-05-29 19:34:57 -0400

    Add files via upload
    Added object to display

--- first_pygame.py
+++ first_pygame.py
@@ -3,7 +3,7 @@ import pygame
 pygame.init()
 
 # Display window of game created
-window = pygame.display.set_mode((500,500))
+win = pygame.display.set_mode((500,500))
 pygame.display.set_caption("Title of Game")
 
 x = 50
@@ -17,10 +17,11 @@ while run:
     pygame.time.delay(100)
 
     for event in pygame.event.get():
-
         # Exits window and terminates application upon quitting program
         if event.type == pygame.QUIT:
             run = False
 
+    pygame.draw.rect(win, (255,0,0), (x, y, width, height))
+    pygame.display.update()
 
 pygame.quit()
