9a001fcbeb5a2f821ed2647e7422134b749cbfd8
xBurrito <awxchang@gmail.com>
2020-05-29 20:25:40 -0400

    Update first_pygame.py
    Implement Jumping functionality

--- first_pygame.py
+++ first_pygame.py
@@ -9,11 +9,18 @@ screenHeight = 500
 win = pygame.display.set_mode((screenWidth, screenHeight))
 pygame.display.set_caption("Title of Game")
 
-x = 50
-y = 50
-width_of_rectangle = 40
-height_of_rectangle = 60
-velocity = 5
+# Red Rectangle properties
+width_of_rectangle = 50
+height_of_rectangle = 75
+
+velocity = 5 # Speed of rectangle
+x = 50  # Initial position on x-axis
+y = 400 # Initial position on y-axis
+
+
+# Jumping Mechanic
+jumping = False
+jump_count = 10
 
 run = True
 while run:
@@ -26,16 +33,35 @@ while run:
 
     keyPress = pygame.key.get_pressed()
 
-    # Created object will move with keypress
+    # Created object will move with keypress (Arrow Keys)
     # Second condition checks to make sure movable object stays within the window's border
     if keyPress[pygame.K_LEFT] and (x > velocity):
       x -= velocity
     if keyPress[pygame.K_RIGHT] and (x < screenWidth - width_of_rectangle):
       x += velocity
-    if keyPress[pygame.K_UP] and (y > velocity):
-      y -= velocity
-    if keyPress[pygame.K_DOWN] and (y < screenHeight - height_of_rectangle):
-      y += velocity
+
+    # Moving rectangle up/down is only allowed when object isn't performing jumping action
+    if not(jumping):
+      if keyPress[pygame.K_UP] and (y > velocity):
+        y -= velocity
+      if keyPress[pygame.K_DOWN] and (y < screenHeight - height_of_rectangle):
+        y += velocity
+      if keyPress[pygame.K_SPACE]:
+        jumping = True
+
+    # Moving left/right is still allowed when jumping motion is in effect
+    else:
+      if jump_count >= -10:
+        neg = 1
+        if jump_count < 0:
+          neg = -1
+
+        # Jumping action is performed with a negative parabola effect
+        y -= (jump_count ** 2) / 2 * neg
+        jump_count -= 1
+      else:
+        jumping = False
+        jump_count = 10
 
     win.fill((0,0,0))
     pygame.draw.rect(win, (255,0,0), (x, y, width_of_rectangle, height_of_rectangle))
