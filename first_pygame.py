ea63da50cae807852f632528063d37cd52f84c2a
xBurrito <awxchang@gmail.com>
2020-05-29 20:04:55 -0400

    Update first_pygame.py
    Prevent object from mmoving outside window border

--- first_pygame.py
+++ first_pygame.py
@@ -3,13 +3,16 @@ import pygame
 pygame.init()
 
 # Display window of game created
-win = pygame.display.set_mode((500,500))
+screenWidth = 500
+screenHeight = 500
+
+win = pygame.display.set_mode((screenWidth, screenHeight))
 pygame.display.set_caption("Title of Game")
 
 x = 50
 y = 50
-width = 40
-height = 60
+width_of_rectangle = 40
+height_of_rectangle = 60
 velocity = 5
 
 run = True
@@ -24,16 +27,18 @@ while run:
     keyPress = pygame.key.get_pressed()
 
     # Created object will move with keypress
-    if keyPress[pygame.K_LEFT]:
+    # Second condition checks to make sure movable object stays within the window's border
+    if keyPress[pygame.K_LEFT] and (x > velocity):
       x -= velocity
-    if keyPress[pygame.K_RIGHT]:
+    if keyPress[pygame.K_RIGHT] and (x < screenWidth - width_of_rectangle):
       x += velocity
-    if keyPress[pygame.K_UP]:
+    if keyPress[pygame.K_UP] and (y > velocity):
       y -= velocity
-    if keyPress[pygame.K_DOWN]:
+    if keyPress[pygame.K_DOWN] and (y < screenHeight - height_of_rectangle):
       y += velocity
 
-    pygame.draw.rect(win, (255,0,0), (x, y, width, height))
+    win.fill((0,0,0))
+    pygame.draw.rect(win, (255,0,0), (x, y, width_of_rectangle, height_of_rectangle))
     pygame.display.update()
 
 pygame.quit()
